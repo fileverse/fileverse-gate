092da7420de6a52a51c336d2dfee988e60022841
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2022-11-14 16:20:30 +0530

    Added Contract Provider and Member Chain Support

--- src/domain/contract/index.js
+++ src/domain/contract/index.js
@@ -1,49 +1,43 @@
-const config = require('../../../config');
 const abi = require('./abi.json');
 const { ethers } = require("ethers");
+const provider = require('./provider');
 
 class SubdomainContract {
-    constructor(contractAddress) {
+    constructor(contractAddress, network) {
         this.contractAddress = contractAddress;
         this.contractABI = abi;
-        this.networkProviderUrl = config.NETWORK_PROVIDER_URL;
+        this.networkProviderUrl = provider.getNetworkUrl(network);
         this.networkProvider = new ethers.providers.JsonRpcProvider(this.networkProviderUrl);
         this.contractInstance = new ethers.Contract(this.contractAddress, this.contractABI, this.networkProvider);
     }
 
     async getFileCount() {
         const fileCount = await this.contractInstance.getFileCount();
-        console.log(fileCount);
         return fileCount;
     }
 
     async getFile(fileId) {
         const file = await this.contractInstance.files(fileId);
-        console.log(file);
         return file;
     }
 
     async getCollaboratorList() {
         const collaboratorList = await this.contractInstance.getCollaborators();
-        console.log(collaboratorList);
         return collaboratorList;
     }
 
     async getCollaboratorCount() {
         const collaboratorCount = await this.contractInstance.getCollaboratorCount();
-        console.log(collaboratorCount);
         return collaboratorCount;
     }
 
     async isCollaborator(address) {
         const isCollaborator = await this.contractInstance.isCollaborator(address);
-        console.log(isCollaborator);
         return isCollaborator;
     }
 
     async isMember(address) {
         const memberDetail = await this.contractInstance.members(address);
-        console.log(memberDetail);
         return memberDetail;
     }
 
@@ -54,8 +48,30 @@ class SubdomainContract {
 
     async isOwner(address) {
         const owner = await this.contractInstance.owner();
-        console.log(owner);
         return address.toLowerCase() === owner.toLowerCase();
+    }
+
+    static networkFromChainId(chainId) {
+        if (!chainId) {
+            return 'eth_goerli';
+        }
+        const chainIdInNumber = Number(chainId);
+        if (chainIdInNumber === 5) {
+            return 'eth_goerli';
+        }
+        if (chainIdInNumber === 8420) {
+            return 'fileverse_testnet';
+        }
+        if (chainIdInNumber === 1) {
+            return 'eth_mainnet';
+        }
+        if (chainIdInNumber === 137) {
+            return 'polygon_mainnet';
+        }
+        if (chainIdInNumber === 100) {
+            return 'gnosis_mainnet';
+        }
+        return 'eth_goerli';
     }
 };
 
