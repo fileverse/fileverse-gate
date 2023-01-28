13093ed90eae97092ac8caac69bc1877225bd2dc
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-01-28 11:45:50 +0530

    Added tokenId to getStatus by Contract API

--- src/domain/contract/getStatus.js
+++ src/domain/contract/getStatus.js
@@ -1,9 +1,13 @@
 const config = require('../../../config');
+const { RegistryContract } = require('../contract');
+const registryInstance = new RegistryContract();
 
 async function getStatus({ contractAddress, invokerAddress }) {
+  const tokenId = await registryInstance.getTokenId(contractAddress);
   return {
     contractAddress,
     invokerAddress,
+    tokenId,
     PORTAL_REGISTRY_ADDRESS: config.PORTAL_REGISTRY_ADDRESS,
     PORTAL_REGISTRY_NETWORK: config.PORTAL_REGISTRY_NETWORK,
     REGISTER_MEMBER_CONTRACT: config.REGISTER_MEMBER_CONTRACT,
