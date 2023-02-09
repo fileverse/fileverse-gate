f54402a97ee6abf6994f6ec040abc36688c549b9
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-02-09 10:58:33 +0530

    Minor Fix

--- src/domain/contract/getStatus.js
+++ src/domain/contract/getStatus.js
@@ -16,7 +16,7 @@ async function getStatus({ contractAddress, invokerAddress, chainId }) {
     invokerAddress,
     chainId,
     tokenId,
-    isMember: !!balance,
+    isMember: !balance.isZero(),
     isCollaborator,
     PORTAL_REGISTRY_ADDRESS: config.PORTAL_REGISTRY_ADDRESS,
     PORTAL_REGISTRY_NETWORK: config.PORTAL_REGISTRY_NETWORK,
