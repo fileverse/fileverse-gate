9e4fab00e38b603ef7012a0d0e52455096f0cef7
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-06-07 15:47:51 +0530

    added api key to auth the request, and added new endpoint to check the existing address

---
+++ src/interface/whitelist/getAddress.js
@@ -0,0 +1,23 @@
+const Whitelist = require('../../domain/whitelist');
+
+async function getAddress(req, res) {
+    const { address } = req.params;
+    try {
+        const whitelist = await Whitelist.find({ address });
+        if (!whitelist) {
+            throw new Error(`Address ${address} not found`);
+        }
+        res.json({
+            success: true,
+            error: null,
+            invokerAddress: whitelist.invokerAddress,
+            tag: whitelist.tag,
+            timeStamp: whitelist.timeStamp,
+        });
+    }
+    catch (error) {
+        return res.status(400).json({ success: false, error: error.message });
+    }
+}
+
+module.exports = getAddress;
--- src/interface/whitelist/index.js
+++ src/interface/whitelist/index.js
@@ -8,7 +8,10 @@ const {
 } = require('../../infra/asyncHandler');
 
 const whitelistAddress = require('./address');
+const getAddress = require('./getAddress');
+const { whitelistAdmin } = require('../middleware');
 
-router.post('/address', asyncHandlerArray(whitelistAddress));
+router.post('/address', asyncHandler(whitelistAdmin), asyncHandlerArray(whitelistAddress));
+router.get('/address/:address', asyncHandler(getAddress));
 
 module.exports = router;
