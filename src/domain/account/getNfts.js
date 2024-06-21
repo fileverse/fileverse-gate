2cdc3b7e860795ba408a2a7b92b6f20ea00d0501
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-06-21 19:50:33 +0530

    ownership confirmation added

--- src/domain/account/getNfts.js
+++ src/domain/account/getNfts.js
@@ -19,6 +19,11 @@ async function getNfts({ address, search, chain, tokenAddress, tokenId }) {
     });
   }
 
+  nfts = nfts.filter((nft) => {
+    // here I want nfts that have 'address' same as the param
+    return nft.owner_of === address;
+  });
+
   // eslint-disable-next-line
   return nfts.filter((nft) => nft.name.match(new RegExp(search, 'i')));
 }
