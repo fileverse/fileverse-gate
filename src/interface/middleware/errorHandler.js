a55147fe984f13d51316831cb2c842f1479de290
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-06-21 16:20:45 +0530

    logg prettified

--- src/interface/middleware/errorHandler.js
+++ src/interface/middleware/errorHandler.js
@@ -2,8 +2,8 @@ const { ValidationError } = require('./validator');
 
 // eslint-disable-next-line no-unused-vars
 function expressErrorHandler(err, req, res, next) {
-    console.log(req.params);
-    console.log(err.details);
+  console.log("req params", req.params);
+  console.log("error details", err.details);
   if (err instanceof ValidationError) {
     return res
       .status(err.statusCode)
